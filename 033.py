9aadf23f2b72a6e8e50ee2876300a17af233f696
Abhishek Vangipuram <abhivangipuram@gmail.com>
2021-12-03 16:27:11 -0500

    probs 33 - 35

--- 033.py
+++ 033.py
@@ -6,11 +6,22 @@ start = time.time()
 arr = []
 for i in range(10, 100):
     for j in range(10, 100):
+        if i >= j:
+            continue
         if not (j % 10 == 0 and i % 10 == 0):
-            if i / j == math.floor(i / 10) / (j % 10):
-                arr.append((i, j))
-            elif i / j == (i % 10) / math.floor(j / 10):
-                arr.append((i, j))
+            a1, a2, b1, b2 = math.floor(i/10), i%10, math.floor(j/10), j%10
+            if a1 == b1 and b2!=0:
+                if i/j == a2/b2:
+                    arr.append((i,j))
+            if a1 == b2:
+                if i/j == a2/b1:
+                    arr.append((i,j))
+            if a2 == b1 and b2!=0:
+                if i/j == a1/b2:
+                    arr.append((i,j))
+            if a2 == b2:
+                if i/j == a1/b1:
+                    arr.append((i,j))
 num = 1
 den = 1
 for (a, b) in arr:
--- useful_functions.py
+++ useful_functions.py
@@ -49,3 +49,23 @@ def factors(n):
 
 def numOfFactors(n):
     return len(factors(n))
+
+def isPrime(n):
+    if n<2:
+        return False
+    if n == 2 or n == 3 or n == 5 or n == 7: 
+        return True
+    if n%2 == 0 or n%3 == 0: 
+        return False
+
+    r = int(n**0.5)
+    # since all primes > 3 are of the form 6n ± 1
+    # start with f=5 (which is prime)
+    # and test f, f+2 for being prime
+    # then loop by 6. 
+    f = 5
+    while f <= r:
+        if n % f == 0: return False
+        if n % (f+2) == 0: return False
+        f += 6
+    return True   
